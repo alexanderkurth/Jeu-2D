a1aff5b89192d3540cde0701d707390d54774e86
KURTH Alexander <38751509+alexanderkurth@users.noreply.github.com>
2018-09-23 21:42:50 +0200

    Debut classe World

--- Jeu 2D/src/main/world/World.java
+++ Jeu 2D/src/main/world/World.java
@@ -1,9 +1,22 @@
 package main.world;
 
+import java.awt.Graphics;
+
 import main.Handler;
 
 public class World {
 	
 	private Handler handler;
 
+	public World(Handler handler, String path) {
+		this.handler = handler;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
 }
